002bf980e212bd2414be1042dfb455a2658114c6
krishpranav <krisna.pranav@gmail.com>
2021-12-30 11:52:54 +0530

    requests: aql[cache + batchsize + bind]

--- requests/aql.go
+++ requests/aql.go
@@ -17,6 +17,27 @@ func NewAQL(query string, params ...interface{}) *AQL {
 	return &AQL{query: processAQL(fmt.Sprintf(query, params...))}
 }
 
+func (a *AQL) Cache(enable bool) *AQL {
+	a.cache = &enable
+	return a
+}
+
+func (a *AQL) BatchSize(size int) *AQL {
+	a.batchSize = size
+	return a
+}
+
+func (a *AQL) Bind(name string, value interface{}) *AQL {
+	if a.bindVars == nil {
+		a.bindVars = make(map[string]interface{})
+	}
+	a.bindVars[name] = value
+	return a
+}
+
+func (a *AQL) Path() string {
+	return "/_api/cursor"
+}
 func processAQL(query string) string {
 	buf := bytes.NewBuffer(nil)
 	space := false
