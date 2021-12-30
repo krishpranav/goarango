2b6a222be68ff8a9453d606a164fed7c1255cdb4
krishpranav <krisna.pranav@gmail.com>
2021-12-30 12:24:36 +0530

    requests: cache

--- requests/cache.go
+++ requests/cache.go
@@ -2,22 +2,34 @@ package requests
 
 import "encoding/json"
 
-type SetCachceProperties struct {
+type SetCacheProperties struct {
 	Mode       string `json:"mode,omitempty"`
 	MaxResults int    `json:"maxResults,omitempty"`
 }
 
-func (r *SetCachceProperties) Path() string {
+func (r *SetCacheProperties) Path() string {
 	return "/_api/query-cache/properties"
 }
 
-func (r *SetCachceProperties) Method() string {
+func (r *SetCacheProperties) Method() string {
 	return "PUT"
 }
 
-func (r *SetCachceProperties) Generate() []byte {
+func (r *SetCacheProperties) Generate() []byte {
 	m, _ := json.Marshal(r)
 	return m
 }
 
 type GetCacheProperties struct{}
+
+func (r *GetCacheProperties) Path() string {
+	return "/_api/query-cache/properties"
+}
+
+func (r *GetCacheProperties) Method() string {
+	return "GET"
+}
+
+func (r *GetCacheProperties) Generate() []byte {
+	return nil
+}
