77ae47a99b12b59133f8cb85f9d73d0c168b6b37
krishpranav <krisna.pranav@gmail.com>
2021-12-30 11:59:07 +0530

    requests: aqlfunction

--- requests/aqlfunction.go
+++ requests/aqlfunction.go
@@ -1,11 +1,13 @@
 package requests
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
 type CreateAQLFunction struct {
 	Name            string `json:"name"`
 	Code            string `json:"code"`
-	IsDeterministic bool
+	IsDeterministic bool   `json:"isDeterministic"`
 }
 
 func (r *CreateAQLFunction) Path() string {
@@ -18,13 +20,52 @@ func (r *CreateAQLFunction) Method() string {
 
 func (r *CreateAQLFunction) Generate() []byte {
 	m, _ := json.Marshal(r)
-
 	return m
 }
 
 type DeleteAQLFunction struct {
 	Name  string
 	Group bool
+}
+
+func (r *DeleteAQLFunction) Path() string {
+	path := "/_api/aqlfunction/" + r.Name
+
+	if r.Group {
+		path += "?group=true"
+	}
+
+	return path
+}
+
+func (r *DeleteAQLFunction) Method() string {
+	return "DELETE"
+}
+
+func (r *DeleteAQLFunction) Generate() []byte {
+	return nil
+}
+
+type GetAQLFunctions struct {
+	Namespace string
+}
+
+func (r *GetAQLFunctions) Path() string {
+	path := "/_api/aqlfunction"
+
+	if r.Namespace != "" {
+		path += "?namespace=" + r.Namespace
+	}
+
+	return path
+}
+
+func (r *GetAQLFunctions) Method() string {
+	return "GET"
+}
+
+func (r *GetAQLFunctions) Generate() []byte {
+	return nil
 }
 
 type AQLFunction struct {
